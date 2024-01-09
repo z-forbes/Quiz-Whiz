9186975708e0a6b6703a4b3d76c33cfeac9f969e
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2024-01-09 17:33:45 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from program.input_parser import parse_input
-import program.learn_exporter, program.moodle_exporter
+import program.learn_exporter as learn_exporter
+import program.moodle_exporter as moodle_exporter
 from os import listdir
 from program.utils import *
 
