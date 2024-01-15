af57a10662a5a1728186379d4f4b9af6042a23b8
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2024-01-15 11:37:58 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ parser.add_argument('--learn', '-l', action='store_true',
                     help='Produce learn output.')
 
 parser.add_argument('--debug', '-d', action='store_true',
-                    help='Show detailed error messages.')
+                    help='Include to show detailed error messages.')
 
 # parser.add_argument('--clear-output', action='store_true', 
 #                     help='Clear output folder before creating new output') 
@@ -67,8 +67,7 @@ def main(args):
     # output
     output_dir = "output"
     # output dirname validation
-    
-    if args.output and path.basename(args.output)=="program":
+    if path.basename(args.output)=="program":
         error("Output directory cannot be the same as program directory")
     if args.output:
         output_dir = args.output
@@ -93,7 +92,7 @@ def main(args):
         if args.moodle:
             moodle(quiz, path.join(output_dir, f"MOODLE_{path.basename(quiz.input_file)}"))
 
-    print(f"{Fore.GREEN}Success!{Fore.RESET}\n")
+    print(f"{Fore.GREEN}Success!{Fore.RESET}")
 
 
 ## EXCECUTION STARTS HERE ##
