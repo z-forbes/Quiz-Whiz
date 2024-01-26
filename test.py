032e1f628839fb927fde41096402b7a11b1d10e0
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2024-01-26 17:05:52 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 from program.input_parser import parse_input
 import program.learn_exporter as learn_exporter
 import program.moodle_exporter as moodle_exporter
-from os import listdir
+import os
 from program.utils import *
 from program.Question import QType
 
-a = QType.MATCH
-print(a.value)
+def count_lines():
+    files = ["main.py"]
+    for f in os.listdir("program/"):
+        if not "pycache" in f:
+            files.append(os.path.join("program/", f))
 
-print("test")
+    total = 0 
+    for p in files:
+        with open(p, "r") as f:
+            total += len(f.readlines())
+    print(total)
+
+count_lines()
