62d623ab032ae65cb1a74ef610abdf3197a37718
Lewis Forbes <60221117+lewisforbes@users.noreply.github.com>
2024-01-30 12:35:26 +0000

    Add files via upload

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import program.moodle_exporter as moodle_exporter
 import os
 from program.utils import *
 from program.Question import QType
+import markdown2
+
 
 def count_lines():
     files = ["main.py"]
@@ -17,4 +19,40 @@ def count_lines():
             total += len(f.readlines())
     print(total)
 
-count_lines()
+# count_lines()
+    
+def md_to_html_pandoc(md_str):
+    # (over)write tmp_md file
+    mk_tmp_dir() 
+    md_fpath = TMP_DIR()+"/md_tmp.md"
+    f = open(md_fpath, "w")
+    f.write(md_str)
+    f.close()
+
+    # use pandoc to create tmp html file
+    html_fpath = TMP_DIR()+"/html_tmp.html"
+    pypandoc.convert_file(md_fpath, 'html', format='md', outputfile=html_fpath)
+
+    # read html file contents
+    f = open(html_fpath, "r", encoding="utf-8")
+    html = f.read()
+    f.close()
+
+    return html
+
+    
+md = '''
+normal text 
+code:
+```python
+def sum(a,b):
+    return a+b
+```
+local image: ![alt text](inputs/images/dot.png)
+web image: ![alt text](https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSai5tNIi1MsKHbPiTwr6-fM4eyCUWwJMZGB7xnQbAy3KNGmxnt){width=100px}
+**bold text** *italic test*
+'''
+with open("delete.html", "w") as f:
+    # f.write(md_to_html_pandoc(md))
+    # f.write(markdown.markdown(md))
+    pass
